a7c112af6593dca5b77e33061e16fd8e6a07fad6
Juan111026 <elpatitosan1026@gmail.com>
2025-08-22 20:50:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -146,8 +146,8 @@ function showCountdown() {
   const container = document.getElementById('countdown');
   let startParam = getURLParam('start');
   let eventParam = getURLParam('event');
-  let startDate = startParam ? new Date(startParam + 'T20:06:2025') : new Date('2025-03-30T00:00:00'); 
-  let eventDate = eventParam ? new Date(eventParam + 'T20:06:2025') : new Date('2025-03-30T00:00:00');
+  let startDate = startParam ? new Date(startParam + 'T00:00:00') : new Date('2025-03-30T00:00:00'); 
+  let eventDate = eventParam ? new Date(eventParam + 'T00:00:00') : new Date('2025-03-30T00:00:00');
 
   function update() {
     const now = new Date();
